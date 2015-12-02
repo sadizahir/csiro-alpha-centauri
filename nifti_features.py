f012027d8a73d059820d96368a643ae1e099550f
Sadi Zahir <sadi.zahir@gmail.com>
2015-12-02 11:35:30 +1000

    Add Gabor kernel generation and basic plotting.

--- nifti_features.py
+++ nifti_features.py
@@ -18,16 +18,29 @@ import matplotlib.pyplot as plt
 # Scientific
 from sklearn.feature_extraction.image import extract_patches_2d
 from sklearn.decomposition import PCA
+from skimage import data
+from skimage.util import img_as_float
+from skimage.filters import gabor_kernel
+from scipy import ndimage as ndi
 
 # Patch Generation Constants
 PATCH_SIZE = 20
-MAX_EIGEN = 25
+MAX_EIGEN = 10
 
 # Debugging stuff
 # Set to 0 if you don't want image-size messages, timing, etc.
 DEBUG = 1
 SHOW_IMG = 1
 TEST = 1
+
+"""
+Image normalisation from the Gabor example.
+"""
+def power(image, kernel):
+    # Normalize images for better comparison.
+    image = (image - image.mean()) / image.std()
+    return np.sqrt(ndi.convolve(image, np.real(kernel), mode='wrap')**2 +
+                   ndi.convolve(image, np.imag(kernel), mode='wrap')**2)
 
 """
 Extracts the data for single image from a NIFTI-encoded medical image.
@@ -144,18 +157,107 @@ def show_eigenpatches(eigens, patch_size):
     plt.figure(figsize=(4.2, 4))
     for i, comp in enumerate(eigens):
         plt.subplot(5, 5, i + 1)
-        plt.imshow(comp.reshape(psize), cmap=plt.cm.gray_r, 
-                   interpolation='nearest')
+        plt.imshow(comp.reshape(psize), cmap=plt.cm.gray, interpolation='nearest')
         plt.xticks(())
         plt.yticks(())
     plt.suptitle('Eigen-decomposition of Patches in ROI\n', fontsize=16)
     plt.subplots_adjust(0.08, 0.02, 0.92, 0.85, 0.08, 0.23)
     plt.show()
+
+"""
+Generates Gabor kernels.
+"""
+def generate_kernels():
+    kernels = []
+    for theta in range(4):
+        theta = theta / 4. * np.pi
+        for sigma in (1, 3):
+            for frequency in (0.05, 0.25):
+                kernel = np.real(gabor_kernel(frequency, theta=theta,
+                                              sigma_x=sigma, sigma_y=sigma))
+                kernels.append(kernel)
+    return kernels
+
+"""
+From a kernel and an image, compute the features of the image.
+"""    
+def compute_feats(image, kernels):
+    feats = np.zeros((len(kernels), 2), dtype=np.double)
+    for k, kernel in enumerate(kernels):
+        filtered = ndi.convolve(image, kernel, mode='wrap')
+        feats[k, 0] = filtered.mean()
+        feats[k, 1] = filtered.var()
+    return feats
+
+"""
+Compares features against reference features.
+"""
+def match(feats, ref_feats):
+    min_error = np.inf
+    min_i = None
+    for i in range(ref_feats.shape[0]):
+        error = np.sum((feats - ref_feats[i, :])**2)
+        if error < min_error:
+            min_error = error
+            min_i = i
+    return min_i
     
+def plot_gabor(images, patch_size):
+    # Plot a selection of the filter bank kernels and their responses.
+    images = images.reshape(images.shape[0], patch_size, patch_size)
+    
+    results = []
+    kernel_params = []
+    for theta in (0, 1):
+        theta = theta / 4. * np.pi
+        for frequency in (0.1, 0.4):
+            kernel = gabor_kernel(frequency, theta=theta)
+            params = 'theta=%d,\nfrequency=%.2f' % (theta * 180 / np.pi, 
+                                                    frequency)
+            kernel_params.append(params)
+            # Save kernel and the power image for each image
+            results.append((kernel, [power(img, kernel) for img in images]))
+    
+    fig, axes = plt.subplots(nrows=5, ncols=11, figsize=(11, 6))
+    plt.gray()
+    
+    fig.suptitle('Image responses for Gabor filter kernels', fontsize=12)
+    
+    axes[0][0].axis('off')
+    
+    # Plot original images
+    for label, img, ax in zip([str(i) for i in range(0, len(images))], 
+                               images, axes[0][1:]):
+        ax.imshow(img, cmap=plt.cm.gray, interpolation='nearest')
+        ax.set_title(label, fontsize=9)
+        ax.axis('off')
+    
+    for label, (kernel, powers), ax_row in zip(kernel_params, results, axes[1:]):
+        # Plot Gabor kernel
+        ax = ax_row[0]
+        ax.imshow(np.real(kernel), cmap=plt.cm.gray, interpolation='nearest')
+        ax.set_ylabel(label, fontsize=7)
+        ax.set_xticks([])
+        ax.set_yticks([])
+    
+        # Plot Gabor responses with the contrast normalized for each filter
+        vmin = np.min(powers)
+        vmax = np.max(powers)
+        for patch, ax in zip(powers, ax_row[1:]):
+            ax.imshow(patch, vmin=vmin, vmax=vmax, interpolation='nearest')
+            ax.axis('off')
+    
+    plt.show()
+
+  
 """
 This is where I test!
 """
 def test_routine():
+    pass
+
+if TEST:
+    test_routine()
     path = ""
     filename = path + "anon_mr_150420_30sec.nii.gz"
     filename_label = path + "anon_mr_150420_30sec.nii-label.nrrd.nii.gz"
@@ -190,9 +292,12 @@ def test_routine():
     # Show the eigens, if you want
     if SHOW_IMG:
         show_eigenpatches(eigens, PATCH_SIZE)
-
-if TEST:
-    test_routine()
+        
+    # Generate Gabor Kernels
+    kernels = generate_kernels()
+    
+    # Show the Gabors
+    plot_gabor(eigens, PATCH_SIZE)
     
     
     
