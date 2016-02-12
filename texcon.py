c31b0729a0409119b68c2516140a14f2da93a625
Sadi Zahir <sadi.zahir@gmail.com>
2016-02-12 14:07:03 +1000

    Add empty file for generalised volume classification script.

---
+++ texcon.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Front-end and class definitions to store classifier and NIFTI information and
+to access helper methods described in the other scripts.
+
+Created on Fri Feb 12 13:52:50 2016
+
+@author: Sadi
+"""
+
